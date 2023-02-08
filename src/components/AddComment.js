2f31c6894a158235dce69bd6742b5e341e5f3dcb
Vincenzo <vincenzochiovaro194@gmail.com>
2023-02-08 08:23:53 +0000

    display formatted date using dayjs

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -1,4 +1,7 @@
+import dayjs from "dayjs";
+
 const AddComment = ({ comments }) => {
+  const date = dayjs(comments.created_at).format("DD-MM-YYYY");
   return (
     <section className="comments">
       <textarea></textarea>
@@ -8,7 +11,7 @@ const AddComment = ({ comments }) => {
           <article className="comment" key={comment.comment_id}>
             <p className="comment__body">{comment.body}</p>
             <p className="comment__info">
-              Created at: {comment.created_at} by {comment.author}
+              Created at: {date} by {comment.author}
             </p>
           </article>
         );
